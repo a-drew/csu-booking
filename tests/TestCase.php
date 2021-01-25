82b3a44d849732f60aac4f156364b70c4617370b
Andrew H <andrew@plankdesign.com>
2021-01-25 14:17:07 -0500

    task(#362): ensure php feature tests run in memory db

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,10 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public static function setUpBeforeClass(): void
+    {
+        putenv('DB_CONNECTION=sqlite');
+        putenv('DB_DATABASE=:memory:');
+    }
 }
